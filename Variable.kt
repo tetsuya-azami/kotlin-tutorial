b082dcd3d24acae23e7e948cd334d75ba3dd218d
tetuya-azami <zqianjian35@gmail.com>
2023-12-22 06:22:58 +0900

    variable tutorial

---
+++ Variable.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+	// var: mutable variable
+	var n = 10
+	n= 15
+	println(n)
+
+	// val: immutable variable
+	val m = 20
+	// m = 25 // error
+	println(m)
+
+	// type annotation
+	var x: Int
+	x = 10
+	println(x)
+}
